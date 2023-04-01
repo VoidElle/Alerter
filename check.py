3b8db4f1bfc88a3ff59f23424c3d50939684eb28
VoidElle <lucadelc@gmail.com>
2023-04-01 12:29:17 +0200

    Added CloudMarkt shop and LDLC shop

--- check.py
+++ check.py
@@ -12,10 +12,7 @@ config_file = open('config.json', encoding='utf-8')
 config_data = json.load(config_file)
 config_file.close()
 
-messages_sent_file = open('messages_sent.json', encoding='utf-8')
-messages_sent_data = json.load(messages_sent_file)
-messages_sent_file.close()
-
+waiting_time = config_data["waiting_time_s"]
 stores = config_data["stores"]
 
 driver = uc.Chrome()
@@ -45,8 +42,8 @@ for store in stores:
 
         products_list = body.find_all(element_to_search, {type_to_search: class_or_id_to_search})
         while products_list is None:
-            print("INFO: Product not found, waiting 5s and restarting the search...")
-            time.sleep(5)
+            print("INFO: Product not found, waiting " + waiting_time + "s and restarting the search...")
+            time.sleep(waiting_time)
             soup = BeautifulSoup(driver.page_source, features="lxml")
             body = soup.body
             products_list = body.find_all(element_to_search, {type_to_search: class_or_id_to_search})
@@ -62,16 +59,17 @@ for store in stores:
 
         product = body.find(element_to_search, {type_to_search: class_or_id_to_search})
         while product is None:
-            print("INFO: Product not found, waiting 5s and restarting the search...")
-            time.sleep(5)
+            print("INFO: Product not found, waiting " + waiting_time + "s and restarting the search...")
+            time.sleep(waiting_time)
             soup = BeautifulSoup(driver.page_source, features="lxml")
             body = soup.body
             product = body.find(element_to_search, {type_to_search: class_or_id_to_search})
         product_element = product.get_text()
 
     if not_in_stock_text not in product_element:
-        print("IT'S IN STOCK")
+        print("INFO: STOCK AVAILABLE ✅")
+        print(product_element)
     else:
-        print("IS NOT IN STOCK")
+        print("INFO: Stock not available ❌")
 
-    print("====================")
+    print("=========================")
